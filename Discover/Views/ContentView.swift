978f4420b35f1c4b0038fa21e63f6dd26689dd71
MAHESHWARAN <maheshwaran@eam360.com>
2024-01-06 18:26:10 +0530

    first commit

---
+++ Discover/Views/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Discover
+//
+//  Created by MAHESHWARAN on 06/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
